01c4ec8e3f18760d6deb8f383bd093f993d73acc
Alex Kruglov <lime4512@mail.ru>
2024-02-29 23:27:52 +0300

    add front page

--- src/pages/frontLearn/FrontLearn.tsx
+++ src/pages/frontLearn/FrontLearn.tsx
@@ -1,9 +1,36 @@
 import styles from './frontLearn.module.scss'
+import { Card } from '../../components/card/Card'
+import { TitleText } from '../../components/titleText/TitleText'
 
 export const FrontLearn = () => {
 	return (
 		<section className={styles.frontLearn}>
-			<h1>Front</h1>
+			<div className={styles.content}>
+				<TitleText
+					title='Frontend'
+					text='Здесь собранна база вопросов изучив которые вы станете достойным разработчиком'
+				/>
+				<div className={styles['card-list']}>
+					<Card
+						url=''
+						img='/image/card/person_1.svg'
+						title='Junior'
+						text='База вопросов для начинающего'
+					/>
+					<Card
+						url=''
+						img='/image/card/group_2.svg'
+						title='Middle'
+						text='База вопросов для знающего'
+					/>
+					<Card
+						url=''
+						img='/image/card/groups_3.svg'
+						title='Senior'
+						text='База вопросов для опытного'
+					/>
+				</div>
+			</div>
 		</section>
 	)
 }
